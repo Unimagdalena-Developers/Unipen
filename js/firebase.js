c3ba4f57dbea94744474409121febca545e0bc71
santiago-jv <santiagoolayojv@gmail.com>
2022-06-01 23:03:52 -0500

    finish

--- js/firebase.js
+++ js/firebase.js
@@ -2,12 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDk6N7Of4lDOvNfXaNX5pesr49HU82NZXM",
-    authDomain: "unipen-fe78e.firebaseapp.com",
-    projectId: "unipen-fe78e",
-    storageBucket: "unipen-fe78e.appspot.com",
-    messagingSenderId: "224519827775",
-    appId: "1:224519827775:web:b9995022096b95ed5e238a"
+    apiKey: "AIzaSyA-Js1e4lAnQXQm4NNRTsHNO9x5knZrQ0M",
+    authDomain: "unipen-89a83.firebaseapp.com",
+    databaseURL: "https://unipen-89a83-default-rtdb.firebaseio.com",
+    projectId: "unipen-89a83",
+    storageBucket: "unipen-89a83.appspot.com",
+    messagingSenderId: "609313831145",
+    appId: "1:609313831145:web:4dc41eca11e2791c3a5878",
+    measurementId: "G-4L4412C36R"
 };
 
 const app = initializeApp(firebaseConfig);
